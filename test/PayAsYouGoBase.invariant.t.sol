8d631fba99b95933c6803d1776f01fe87a1b163b
egpivo <egpivo@gmail.com>
2025-12-15 19:55:55 -0800

    fix: add forge-lint disable comments for safe uint160 typecasts in PayAsYouGoBase.invariant.t.sol
    - Add forge-lint: disable-next-line(unsafe-typecast) comments for uint160 casts
    - Casts are safe because values (0x1000-0x1004, 0x2000-0x2004) are well within uint160 range
    - Fixes unsafe-typecast warnings at lines 113 and 117

--- test/PayAsYouGoBase.invariant.t.sol
+++ test/PayAsYouGoBase.invariant.t.sol
@@ -110,10 +110,13 @@ contract PayAsYouGoBaseInvariantTest is Test {
             // forge-lint: disable-next-line(unsafe-typecast)
             // casting to 'uint160' is safe because we're only using small values (0x1000-0x1004, 0x2000-0x2004)
             // which are well within uint160 range (max 0xffffffffffffffffffffffffffffffffffffffff)
-            providers.push(address(uint160(0x1000 + i)));
-            // forge-lint: disable-next-line(unsafe-typecast)
             // casting to 'uint160' is safe because we're only using small values (0x1000-0x1004, 0x2000-0x2004)
             // which are well within uint160 range (max 0xffffffffffffffffffffffffffffffffffffffff)
+            // forge-lint: disable-next-line(unsafe-typecast)
+            providers.push(address(uint160(0x1000 + i)));
+            // casting to 'uint160' is safe because we're only using small values (0x1000-0x1004, 0x2000-0x2004)
+            // which are well within uint160 range (max 0xffffffffffffffffffffffffffffffffffffffff)
+            // forge-lint: disable-next-line(unsafe-typecast)
             users.push(address(uint160(0x2000 + i)));
         }
         
